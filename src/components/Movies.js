72d285b1c213efb6bb42b5c94187bcd4b46b14a7
Cloudymolecule <baires82@gmail.com>
2021-04-29 13:14:02 -0400

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,9 +4,17 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map(movie =>
+          <div key={movie.title + movie.time}>
+            {movie.title}
+            {movie.time}
+            <ul>
+              {movie.genres.map(genre => <li>{genre}</li>)}
+            </ul>
+          </div>)}
     </div>
-  );
-};
+  )
+}
 
 export default Movies;
